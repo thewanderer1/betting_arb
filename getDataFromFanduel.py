2bae008cb7a54fffd4ac9c9d3cec373c20dfc25e
roboticsalp <alpanamr@gmail.com>
2021-05-16 16:42:18 -0400

    Clean up

--- getDataFromFanduel.py
+++ getDataFromFanduel.py
@@ -1,17 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver import Chrome
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-#from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-import random
 import time
 import twitter
-import pudb
 from fanduelBot import FanduelBot
 from barstoolUpcomingBot import BarstoolUpcomingBot
 from barstoolLiveBot import BarstoolLiveBot
