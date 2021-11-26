8afca54aae0d27314df4252de3da85fe2dd3d6e7
thewanderer1 <thewanderer1@users.noreply.github.com>
2021-11-26 17:17:37 -0500

    fixed william hill

--- WilliamHillBot.py
+++ WilliamHillBot.py
@@ -14,25 +14,26 @@ class WilliamHillBot(ScraperBot):
 
         whsoup = BeautifulSoup(self.driver.page_source, 'lxml')
         nfl_teams_html = whsoup.find('div',class_='Expander has--toggle competitionExpander') #there are two of these, one for NFL and one for NCAA - NFL is always first
-        events = nfl_teams_html.find_all('div', class_='eventList')
+        events = nfl_teams_html.find_all('div', class_='EventCard')
         self.teams.clear()
         self.odds.clear()
 
         for e in events:
             wh_teams_selector = e.find_all('div', class_='teamNameContainer')
-            wh_odds_selector = e.find_all('div', class_='oddsView')
+            wh_odds_selector = e.find_all('div', class_='odds truncateText')
             tsl = 0
             osl = 0
             for t in wh_teams_selector:
                 self.teams.append(t.span.get_text().strip())
                 tsl+=1
-
-            for p in wh_odds_selector:
-                value = p.find('div',class_='odds truncateText')
-                osl += 1
-                if value is None:
-                    self.odds.append(0)
-                else:
-                    self.odds.append(int(value.get_text().strip()))
+            if len(wh_odds_selector) < 6:
+                self.odds.append(0)
+                self.odds.append(0)
+            else:
+                for p in wh_odds_selector:
+                    sb = p.find_previous_siblings()
+                    if not sb:
+                        osl += 1
+                        self.odds.append(int(p.get_text().strip()))
 
 
